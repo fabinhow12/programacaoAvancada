1826a6a0bd5559ccf65eb957ebd3fd301eeb1553
fabiosiqueira12 <fabio.siqueira1222@gmail.com>
2016-08-30 00:05:19 -0300

    corrigido todos os bugs

--- Aula03-AbstractFactory/src/repositorios/RepositorioVenda.java
+++ Aula03-AbstractFactory/src/repositorios/RepositorioVenda.java
@@ -26,7 +26,9 @@ public class RepositorioVenda {
            
         if (instancia == null){
             synchronized (RepositorioVenda.class){
+                if (instancia == null){
                 instancia = new RepositorioVenda();
+                }
             }
         }
         
